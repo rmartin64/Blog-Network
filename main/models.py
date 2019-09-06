814c4608572e6e37a31f486cb800bf9a6d662443
rmartin64 <42323211+rmartin64@users.noreply.github.com>
2019-09-05 21:12:19 -0500

    trying again

--- main/models.py
+++ main/models.py
@@ -15,9 +15,9 @@ def load_user(id):
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120), index=True, unique=True)
-    password_hash = db.Column(db.String(128))
+    username = db.Column(db.Text(), index=True, unique=True)
+    email = db.Column(db.Text(), index=True, unique=True)
+    password_hash = db.Column(db.Text())
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     comments = db.relationship('Comment', backref='author', lazy='dynamic')
     about_me = db.Column(db.Text())
